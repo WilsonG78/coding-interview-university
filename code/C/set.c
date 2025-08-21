c78129f5232f59a28368a3af87c848c5376347b4
Filip <latawiecfilip@gmail.com>
2025-08-21 09:50:28 +0200

    set insert added

--- code/C/set.c
+++ code/C/set.c
@@ -1,12 +1,13 @@
-#define max(a,b) ((a) > (b) ? (a) : (b))
 #include <stdlib.h>
 #include <math.h>
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
 typedef struct Node
 {
     int val;
-    Node *left;
-    Node *right;
-    Node *parent;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
     int height;
 }Node;
 
@@ -16,15 +17,34 @@ typedef struct set
 
 }set;
 
+//private
+Node* init_node( int val , Node *left , Node *right,Node *parent){
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if(!new_node){
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
+    new_node->val = val;
+    new_node->left = left;
+    new_node->right = right;
+    new_node->parent = parent;
+    int left_h = left ? left->height : 0;
+    int right_h = right ? right->height : 0;
+    new_node->height = (left_h > right_h ? left_h : right_h) + 1;
+    return new_node;
+}
 
-
+//private
 void update_height(Node *node){
     if( node){
-        node->height  = max(node->left->height , node->right->height) +1;
+        int left_h = node->left ? node->left->height : 0;
+        int right_h = node->right ? node->right->height : 0 ;
+        node->height  = max(left_h,right_h) +1;
     }
     
 }
 
+//private
 void update_height_upwards(Node *node) {
     while (node) {
         int left_h = node->left ? node->left->height : 0;
@@ -33,13 +53,19 @@ void update_height_upwards(Node *node) {
         node = node->parent;
     }
 }
+//private
+void balance_node(set *tree , Node *node){
+    int left_h = node->left ? node->left->height : 0;
+    int right_h = node->right ? node->right->height : 0;
 
-void balance(set *tree , Node *node){
-    if(abs(node->left->height - node->right->height) <2 ){
+    if(abs(left_h - right_h) <2 ){
         return;
     }
-    if(node->left->height - node->right->height >1){
-        if(node->left->right->height > node->left->left->height){
+    if(left_h - right_h >1){
+        int left_right_h = node->left->right ? node->left->right->height : 0;
+        int left_left_h = node->left->left ? node->left->left->height : 0;
+
+        if(left_right_h > left_left_h){
            left_rotate(tree,node->left);
         }
         else{
@@ -48,17 +74,30 @@ void balance(set *tree , Node *node){
         
     }
     else{
-        if(node->right->left->height > node->right->right->height){
+        int right_left_h = node->right->left ? node->right->left->height : 0;
+        int right_right_h = node->right->right ? node->right->right->height : 0;
+
+        if(right_left_h > right_right_h){
             right_rotate(tree,node->right);
         }
         else{
             left_rotate(tree,node);
-        }
-        
+        }  
     }
 
 }
 
+//private
+void balance_tree(set *tree, Node *node){
+    while (node)
+    {
+        update_height(node);
+        balance_node(tree,node);
+        node = node->parent;
+    }
+    
+}
+//private
 void right_rotate(set *tree ,Node *node){
     
     Node *new_root = node->left;
@@ -78,13 +117,14 @@ void right_rotate(set *tree ,Node *node){
         else{
             new_root->parent->right = new_root;
         }
+    }else{
+        tree->root = new_root;
     }
     update_height(node);
     update_height(new_root);
-    update_height_upwards(node->parent);
 
 }
-
+//private
 void left_rotate(set *tree ,Node *node){
     Node *new_root = node->right;
     Node *tmp = new_root->left;
@@ -103,42 +143,56 @@ void left_rotate(set *tree ,Node *node){
         else{
             new_root->parent->right = new_root;
         }
+    }else{
+        tree->root = new_root;
     }
     update_height(node);
     update_height(new_root);
-    update_height_upwards(node->parent);
 }
-
-void insert(set *tree,int val){
-    Node *curr = tree->root;
-    while(1){
-        if( curr->val < val){
-            if(!curr->right){
-                Node *new_node = (Node*)malloc(sizeof(Node));
-                new_node->parent = curr;
-                new_node->height =1;
-                new_node->val = val;
-                curr->right = new_node;
-                return;
-            }
+//private
+Node *next_node_in_post_order(set *tree , Node *node){
+    if(node->right){
+        Node *curr = node->right;
+        while(curr->left){
+            curr = curr->left;
         }
-        if(curr->val >val){
-            if(!curr->left){
-                Node *new_node = (Node*)malloc(sizeof(Node));
-                new_node->parent = curr;
-                new_node->height =1;
-                new_node->val = val;
-                curr->left = new_node;
-                return;
-            }
-        }
-        if(curr->val == val){
-            return;
-        }
+        return curr;
     }
 }
 
-int contains(set *tree , int val){
+
+//public
+void set_insert(set *tree,int val){
+    Node *curr = tree->root;
+    Node *new_node = init_node(val,NULL,NULL,NULL);
+    if(!curr){
+        tree->root = new_node;
+        return;
+    }
+    while(curr){
+        if( curr->val < val){
+            if(!curr->right){
+                new_node->parent = curr;
+                curr->right = new_node;
+                curr = NULL;
+            }
+        }
+        else if(curr->val >val){
+            if(!curr->left){
+                new_node->parent = curr;
+                curr->left = new_node;
+                curr = NULL;
+            }
+        }
+        else{
+            free(new_node);
+            curr =NULL;
+        }
+    }
+    balance_tree(tree,new_node);
+}
+//public
+int set_contains(set *tree , int val){
     Node *curr = tree->root;
     while(curr){
         if(curr->val == val){
@@ -154,3 +208,30 @@ int contains(set *tree , int val){
     return 0;
 }
 
+//public
+//TODO
+void set_delete(set *tree , int val){
+    Node *curr = tree->root;
+    while(curr){
+        if(curr->val  > val){
+            curr= curr->left;
+        }
+        else if(curr->val < val){
+            curr = curr->right;
+        }
+        else{
+            break;
+        }
+    }
+    if(curr){
+        if(curr->right && curr->left){
+            Node *post_order = next_node_in_post_order(tree,curr);
+            curr->val = post_order->val;
+            if(post_order->parent->left == post_order){
+                post_order->right = post_order->parent->left;
+                post_order->right->parent = post_order->parent;
+                free(post_order);
+            }
+        }
+    }
+}
